990e1346b54d120c35f8d042e614a89b81245e82
Antosser <anton.moonalien@gmail.com>
2021-10-28 11:50:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+$("#packs").val("4,6,9,20");
+
+var possible = (n, packs) => {
+	if (n <= 0)
+		return null;
+	if (packs.includes(n))
+		return [n];
+	for (let i = 0; i < packs.length; i++) {
+		if (possible(n - packs[i], packs))
+			return possible(n - packs[i], packs).concat([packs[i]]);
+	}
+	return null;
+}
+
+let often = (arr, n) => {
+	let result = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == n) 
+			result++;
+	}
+	return result;
+}
+
+$("#calculate").click(() => {
+	packs = $("#packs").val().split(',');
+	for (let i = 0; i < packs.length; i++)
+		packs[i] = parseInt(packs[i]);
+	packs.sort((a, b) => a - b);
+	packs.reverse();
+	if (!$("#nuggets").val())
+		return;
+	let set = possible(parseInt($("#nuggets").val()), packs);
+	set.reverse();
+
+	$("#output").empty();
+	if (set) {
+		let table = $("<table />");
+		for (let i = 0; i < packs.length; i++) {
+			tr = $("<tr />").append($("<td />").append(packs[i]));
+			tr.append($("<td />").append(often(set, packs[i])));
+			table.append(tr);
+		}
+		$("#output").append(table);
+	}
+	else {
+		$("#output").append($("<div>Not possible</div>").css("margin", "5px").css("color", "red"));
+	}
+	//$("#output").append($("<div></div>").append("Hello"));
+});
